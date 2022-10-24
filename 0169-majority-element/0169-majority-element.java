9fbc61e831602978d4e13073c293ea6051c79cca
Dipak Kawale <88080690+iamdkcoder@users.noreply.github.com>
2022-10-24 11:38:21 -0700

    Time: 9 ms (50.64%), Space: 57.5 MB (5.22%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,36 @@
 class Solution {
     public int majorityElement(int[] n) {
-     Arrays.sort(n);
-        int count=0;
-        int maxcount=0;
-        int majority=n[0];
-        for(int i=0; i<n.length-1;i++){
-            if(n[i]==n[i+1]){
+      int count=0;
+      int major = n[0];
+        for(int a:n){
+            if(count==0){
                 count++;
-                if(count>maxcount){
-                    majority=n[i];
-                    maxcount=count;
-                }
+                major=a;
+            }else if(major==a){
+                count++;
             }else{
-                count=0;
-
+                count--;
             }
         }
-        return majority;
+        return major;
+        
     }
 }
+
+//o(nlogn) o(1)
+// Arrays.sort(n);
+//         int count=0;
+//         int maxcount=0;
+//         int majority=n[0];
+//         for(int i=0; i<n.length-1;i++){
+//             if(n[i]==n[i+1]){
+//                 count++;
+//                 if(count>maxcount){
+//                     majority=n[i];
+//                     maxcount=count;
+//                 }
+//             }else{
+//                 count=0;
+
+//             }
+//         }
