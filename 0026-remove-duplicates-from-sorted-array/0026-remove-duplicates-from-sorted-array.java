23705b19f52f08f1634287b9f7d517681916ae3c
Dipak Kawale <88080690+iamdkcoder@users.noreply.github.com>
2022-11-11 00:32:32 -0800

    Time: 2 ms (73.26%), Space: 47.9 MB (45.32%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,24 +1,49 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        int i=-1;
-        for(int j=0; j<nums.length;j++){
-            if(set.contains(nums[j])){
-                nums[i]=nums[j];
- 
-                
-            }else{
-                set.add(nums[j]);
+    
+   public int removeDuplicates(int[] nums) {
+       int i=0;
+       for(int j=1; j<nums.length;j++){
+           if(nums[j]==nums[j-1]){
                 i++;
-
-                nums[i]=nums[j];
-            }
-           
-        }
-        
-        for(int k=i+1;k<nums.length;k++){
-            nums[k]=0;
-        }
-        return i+1;
-    }
+           }else{
+               nums[j-i]=nums[j];
+              
+           }
+       }
+       
+      
+        return nums.length-i;
+       
+   }
 }
+
+// int count = 0;
+// for(int i = 1; i < n; i++){
+//     if(A[i] == A[i-1]) count++;
+//     else A[i-count] = A[i];
+// }
+// return n-count;
+
+
+//using set and loops
+
+// public int removeDuplicates(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+//         int i=-1;
+//         for(int j=0; j<nums.length;j++){
+//             if(set.contains(nums[j])){
+//                 nums[i]=nums[j];
+                
+//             }else{
+//                 set.add(nums[j]);
+//                 i++;
+//                 nums[i]=nums[j];
+//             }
+           
+//         }
+        
+//         for(int k=i+1;k<nums.length;k++){
+//             nums[k]=0;
+//         }
+//         return i+1;
+//     }
