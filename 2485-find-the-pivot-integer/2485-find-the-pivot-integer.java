c648fb6f1285625f7504de72c34ea23ce6132379
Dipak Kawale <88080690+iamdkcoder@users.noreply.github.com>
2022-11-30 07:18:26 -0800

    Time: 1172 ms (5.14%), Space: 72.7 MB (5.14%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int sum = (n*(n+1))/2;
+        int presum=0;
+        for(int i=1;i<=n;i++){
+            presum+=i;
+            System.out.println("this is presum "+presum+" for "+i);
+            System.out.println("our calculation is "+(sum-presum+i));
+            if(sum-presum+i==presum){
+                return i;
+            }
+        }
+        return -1;
+            
+    }
+}
